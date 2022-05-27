66b1250d7900905d81e2586354532c0ed904f474
costinha7 <rogercostinha73@gmail.com>
2022-05-26 22:13:05 -0300

    grafico arrumado

--- front-end/home_conteudo.php
+++ front-end/home_conteudo.php
@@ -1,37 +1,49 @@
+<div class="container-fluid">
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
-    
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
+  <div id="graficoPizza"></div>
 
-    <div id="graficoPizza"></div>
-
-    <script type="text/javascript">
-    google.charts.load("current", {packages:["corechart"]});
+  <script type="text/javascript">
+    google.charts.load("current", {
+      packages: ["corechart"]
+    });
     google.charts.setOnLoadCallback(drawChart);
+
     function drawChart() {
       var data = google.visualization.arrayToDataTable([
-        ["Element", "Density", { role: "style" } ],
+        ["Element", "Density", {
+          role: "style"
+        }],
         ["Livros Cadastrados", 12000, "red"],
         ["Livros Alugados", 6000, "green"],
       ]);
 
       var view = new google.visualization.DataView(data);
       view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
+        {
+          calc: "stringify",
+          sourceColumn: 1,
+          type: "string",
+          role: "annotation"
+        },
+        2
+      ]);
 
       var options = {
         title: "Grafico de Livros",
         width: 1270,
         height: 720,
-        bar: {groupWidth: "25%"},
-        legend: { position: "none" },
+        bar: {
+          groupWidth: "25%"
+        },
+        legend: {
+          position: "none"
+        },
       };
       var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
       chart.draw(view, options);
-  }
+    }
   </script>
-<div id="barchart_values" style="width: 900px; height: 300px;"></div>
+  <div id="barchart_values" style="width: 900px; height: 300px;"></div>
+</div>
