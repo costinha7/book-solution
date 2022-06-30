d3266d5f5e6de667d1a0e29fba3a98dfe7c6779f
lucasmenchon <it.lucas@outlook.com>
2022-06-29 21:38:04 -0300

    att

--- cadastro_cliente.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-include "Layout.php";
-$layout = new Layout();
-$layout->conteudo = "front-end/formulario_cliente";
-$layout->index();
--- livros.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "Layout.php";
-$layout = new Layout();
-$layout->conteudo = "front-end/livros_conteudo";
-$layout->index();
